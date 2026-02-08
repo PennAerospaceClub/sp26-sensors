016bf65329e38c3852e3fd39766529dfcb9bdd85
violinmeiling <violinmeiling@gmail.com>
2026-02-08 14:15:11 -0500

    Update imu_test.ino
    Added initialization for I2C and improved serial wait logic.

--- imu_test.ino
+++ imu_test.ino
@@ -5,18 +5,31 @@
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
+//wiring is vin to 3.3v, gnd to gnd, sda to sda, scl to scl
+
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial && millis() < 3000);
+
+  Wire.begin();           // IMPORTANT on Uno R4
+  delay(1000);            // let I2C settle
 
   if (!bno.begin()) {
     Serial.println("No BNO055 detected!");
     while (1);
   }
 
+  delay(1000);            // let sensor boot
+
+  bno.setMode((adafruit_bno055_opmode_t)0x0C);
+  delay(100);
+
   bno.setExtCrystalUse(true);
+
   Serial.println("BNO055 ready!");
 }
+
+
 
 void loop() {
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
